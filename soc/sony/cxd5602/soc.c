08a4396375605147cc6ff99d2167ca3417c8a45a
Bram Vlerick <bram.vlerick@openpixelsystems.org>
2024-05-03 16:33:45 +0200

    soc: sony: add initial cxd5602 soc integration
    Add initial skeleton on the soc layer for the sony cxd5602 soc
    
    Signed-off-by: Bram Vlerick <bram.vlerick@openpixelsystems.org>

---
+++ soc/sony/cxd5602/soc.c
@@ -0,0 +1,9 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+
+#include <cmsis_core.h>
+
+void z_arm_platform_init(void)
+{
+}
+
---
+++ soc/sony/cxd5602/soc.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024 Open Pixel Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef _SONY_CXD5602__H_
+#define _SONY_CXD5602__H_
+
+#endif /* _SONY_CXD5602__H_ */
